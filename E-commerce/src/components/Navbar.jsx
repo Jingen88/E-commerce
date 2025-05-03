a5675f9ebee43ffc033bd3178742c61600377b5b
Tamas H <157651056+Jingen88@users.noreply.github.com>
2025-05-03 14:14:57 +0100

    ok

---
+++ E-commerce/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {assets} from '../assets/frontend_assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center py-5 font-medium '>
+        <img src={assets.logo} className='w-1/2 h-1/2' alt='logo'/>
+        <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+<NavLink className='flex flex-col items-center gap-1'>
+    <p>Home</p>
+    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+</NavLink>
+        </ul>
+        
+    </div>
+  )
+}
+
+export default Navbar
