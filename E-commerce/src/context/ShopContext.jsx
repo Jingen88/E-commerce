3b81ad99ff496abcbb748363a6b735760404e953
Tamas H <157651056+Jingen88@users.noreply.github.com>
2025-05-12 14:19:06 +0100

    ok

--- E-commerce/src/context/ShopContext.jsx
+++ E-commerce/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { use } from "react";
 import { createContext} from "react";
 import {products} from "../assets/frontend_assets/assets";
 
@@ -11,11 +11,43 @@ const currency = '£';
 const delivery_fee = 4.99; 
 const[search,setSearch] = React.useState('')
 const[showSearch, setShowSearch] = React.useState(true)
+const [cartItems, setCartItems] = React.useState([])
+
+   
+
+    const addToCart =  async (itemId,size) => {
+
+    let cartData = structuredClone(cartItems)
+
+    if(cartData[itemId]){
+        if(cartData[itemId][size] === size){
+        cartData[itemId][size] += 1
+    }
+    else{
+        cartData[itemId][size] = 1;
+    }
+}
+    else{
+        cartData[itemId] = {};
+        cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData)
+    }
+
+
+useEffect(() => {
+
+    const cartData = localStorage.getItem('cartItems')
+
+},[cartItems])
+
 
     const value ={
-    products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
-    }
+        products, currency, delivery_fee,
+            search, setSearch, showSearch, setShowSearch,
+            cartItems, addToCart,
+            
+        }
 
     return(
         <ShopContext.Provider value={value}>
