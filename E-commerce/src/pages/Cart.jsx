9831bad2b2ad335892861ca8de26092255e21af3
Tamas H <157651056+Jingen88@users.noreply.github.com>
2025-05-13 11:18:37 +0100

    ok

--- E-commerce/src/pages/Cart.jsx
+++ E-commerce/src/pages/Cart.jsx
@@ -1,9 +1,14 @@
 import React, { use } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import { useEffect } from 'react'
+import Title from '../components/Title'
+import {assets} from '../assets/frontend_assets/assets'
+import CartTotal from '../components/CartTotal'
+
 
 const Cart = () => {
 
-const {products,currency,cartItems} = React.useContext(ShopContext);
+const {products,currency,cartItems,updateQuantity} = React.useContext(ShopContext);
 
 const [cartData, setCartData] = React.useState([]);
 
@@ -11,30 +16,63 @@ const [cartData, setCartData] = React.useState([]);
 
 useEffect(() => {
 
+  const tempData = [];
 for (const items in cartItems){
-    for (const size in cartItems[items]) {
-        try {
-            if (cartItems[items][size] > 0) {
-                const product = products.find((product) => product.id === items);
-                if (product) {
-                    setCartData((prevData) => [
-                        ...prevData,
-                        { ...product, size: size, quantity: cartItems[items][size] },
-                    ]);
-                }
-            }
-        } catch (error) {
-            console.error("Error in getCartCount:", error);
-        }
-    }   
-}
-
-
-
+    for (const item in cartItems[items]) {
+        
+            if (cartItems[items][item] > 0) {
+                tempData.push({
+                    id: items,
+                    size: item,
+                    quantity: cartItems[items][item],
+                }); 
+                
+    }}}
+    setCartData(tempData)
 },[cartItems])
 
   return (
-    <div>Cart</div>
+    <div className='border-t pt-14'>
+      <div className='text-2xl mb-3'>
+      <Title text1={'YOUR'} text2={'CART'}/>
+      </div>
+      <div>
+        {
+          cartData.map((item,index)=>{
+
+            const productData = products.find((product) => product._id === item.id);
+
+            if (!productData) {
+              return null; 
+            }
+
+            return(
+              <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+                <div className='flex items-start gap-6'>
+                  <img className='w-16 sm:w-20' src={productData.image[0]} alt='product image'/>
+                  <div>
+                    <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+                    <div className='flex items-center gap-5 mt-2'>
+                    <p>{currency}{productData.price}</p>
+                    <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
+                    </div>
+                  </div>
+
+                  </div>
+              <input onChange={(e)=> e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item.id,item.size,Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type='number' min={1} defaultValue={item.quantity}/>
+              <img onClick={() => updateQuantity(item.id, item.size)}  className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt='bin icon'/>
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal/>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
