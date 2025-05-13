8fabbd6de2827f6f9a054f07a8003821dc6d6842
Tamas H <157651056+Jingen88@users.noreply.github.com>
2025-05-13 12:40:41 +0100

    ok

--- E-commerce/src/components/Navbar.jsx
+++ E-commerce/src/components/Navbar.jsx
@@ -36,8 +36,9 @@ const{setShowSearch,getCartCount}= useContext(ShopContext)
         </ul>
         <div className='flex items-center gap-6'>
         <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt='search icon'/>
+       
         <div className='group relative'>
-        <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="profile icon"/>
+        <Link to='/login'><img className='w-5 cursor-pointer' src={assets.profile_icon} alt="profile icon"/></Link>
         <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
           <div className='flex flex-col gap-2 w-36 p-3 bg-slate-100 text-gray-500 rounded'>
             <p className='cursor-pointer hover:text-black'>My Profile</p>
