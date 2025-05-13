22d72195df25406ed1320df0887e212f28d5f02f
Tamas H <157651056+Jingen88@users.noreply.github.com>
2025-05-13 13:17:19 +0100

    ok

--- E-commerce/src/pages/About.jsx
+++ E-commerce/src/pages/About.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import Title from '../components/Title'
+import {assets} from '../assets/frontend_assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+
+<div className='my-10 flex flex-row md:flex-row gap-16'>
+  <img className='w-full md:max-w-[450px]' src={assets.about_img} alt=' about image'/>
+  <div className='flex flex-col gap-6 justify-center md:w-2/4'>
+    <p>Welcome to Szaloki, your online destination for unique and expressive apparel. We believe that clothing is more than just fabric; it's a canvas for your personality and a way to connect with the world around you. Our collections are thoughtfully curated, drawing inspiration from art, culture, and the beauty of everyday life. We strive to offer pieces that spark joy, encourage individuality, and stand out from the ordinary.</p>
+    <p>At Szaloki, we are passionate about quality and craftsmanship. We carefully select materials and work with skilled partners to bring our designs to life. Beyond the garments themselves, we are committed to providing a positive and seamless shopping experience for every customer. We hope you find something special that resonates with your personal style and helps you express your authentic self.</p>
+  <b className='text-gray-800'>Our Mission</b>
+  <p>At Szaloki, our mission is to empower individuality through thoughtfully designed and carefully curated apparel. We strive to create pieces that inspire self-expression, celebrate creativity, and bring a touch of artistry to everyday style. By focusing on quality craftsmanship and unique aesthetics, we aim to provide our customers with garments that not only look good but also resonate with their personal stories and aspirations.</p>
+  </div>
+</div>
+<div className='text-4xl py-4'>
+    <Title text1={'WHY'} text2={'CHOOSE US'}/>
+</div>
+
+<div className='flex flex-col md:flex-row gap-8 text-sm mb-20'>
+<div className='border px-10 md:px-16  py-20 flex flex-col gap-5'>
+  <b>Quality Assurance</b>
+  <p className='text-gray-600'>We are committed to providing our customers with apparel that meets the highest standards of quality, from the selection of fabrics to the precision of the stitching.</p>
+</div>
+<div className='border px-10 md:px-16  py-20 flex flex-col gap-5'>
+  <b>Convenience</b>
+  <p className='text-gray-600'>We strive to make your shopping experience as seamless and convenient as possible, offering user-friendly navigation, secure payment options, and flexible delivery choices.</p>
+</div>
+<div className='border px-10 md:px-16  py-20 flex flex-col gap-5'>
+  <b>Exceptional Costumer Service</b>
+  <p className='text-gray-600'>At Szaloki, we are dedicated to providing exceptional customer service, ensuring that your experience is both enjoyable and satisfying from start to finish.</p>
+</div>
+</div>
+
+<NewsletterBox/>
+
+
+    </div>
   )
 }
 
--- E-commerce/src/pages/Contact.jsx
+++ E-commerce/src/pages/Contact.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Title from '../components/Title'
+import {assets} from '../assets/frontend_assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+    <div className='text-center text-2xl pt-10 border-t'>
+          <Title text1="Contact" text2="Us" />
+    </div>
+    <div className=' my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+      <img src={assets.contact_img} className='w-full md:max-w-[400px]' alt='picture of a desk with a laptop'/>
+<div className='flex flex-col justify-center items-start gap-6'>
+  <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+  <p className='text-gray-500'>B17 9MV, Buccaneers Row <br/> Birmingham, West Midlands, UK</p>
+  <p className='text-gray-500'> Tel: +44 780-45-90002 <br/> Email: Szaloki@info.net</p>
+  <p className='font-semibold text-xl text-gray-600'> Carreers at Szaloki</p>
+  <p className='text-gray-500'>Learn more about our teams and job openings </p>
+  <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transiton-all duration-500'>Explore Jobs</button>
+</div>
+    </div>
+    <NewsletterBox/>
+    </div>
   )
 }
 
